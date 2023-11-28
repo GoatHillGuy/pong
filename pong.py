46e78d082ca4bbcb7fff5c6847b09572be3aef6a
Finn Cortesi <finn.cortesi@gmail.com>
2023-11-29 00:25:09 +1300

    Begun on powerups

--- pong.py
+++ pong.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import pygame
+import random
 
 pygame.init()
 pygame.font.init()
@@ -10,6 +11,9 @@ SC_WIDTH = 1200
 SC_HEIGHT = 700
 PAUSED = False
 PFUNC = 0
+spawn = False
+
+running = True
 
 background_colour = (255, 154, 246)
 paddle1_colour = (255, 154, 246)
@@ -17,6 +21,7 @@ paddle2_colour = (255, 255, 153)
 ball_colour = (154, 195, 255)
 score1 = 0
 score2 = 0
+clock = pygame.time.Clock()
 
 pspeed = 20
 
@@ -34,8 +39,15 @@ ballx = 600
 bally = 350
 ballw = 30
 ballh = 30
-BAXM = 1
-BAYM = 1
+BAXM = 3
+BAYM = 3
+
+pux = 600
+puy = 350
+puw = 45
+puh = 45
+PUXM = 2
+PUYM = 2
 
 # Define the dimensions of
 # screen object(width,height)
@@ -53,28 +65,59 @@ blip = pygame.mixer.Sound('./sounds/blip.wav')
 score = pygame.mixer.Sound('./sounds/score.wav')
 victory = pygame.mixer.Sound('./sounds/victory.wav')
 logo = pygame.image.load('./sprites/logo.png')
+smallerlogo = pygame.transform.scale(logo, (330, 180))
+
+text1 = fontMedium.render(str(score1), True, (255, 154, 246))
+textRect1 = text1.get_rect()
+textRect1.center = (400, 50)
+
+text2 = fontMedium.render(str(score2), True, (255, 255, 253))
+textRect2 = text2.get_rect()
+textRect2.center = (800, 50)
+
+textv1 = fontMedium.render("Player 1 has won. Woohoo.", True, (0, 0, 0))
+textRectv1 = textv1.get_rect()
+textRectv1.center = (600, 350)
+
+textv2 = fontMedium.render("Player 2 has won. Yippee", True, (0, 0, 0))
+textRectv2 = textv2.get_rect()
+textRectv2.center = (600, 350)
+
+textr = fontSmall.render("Press X to replay", True, (0, 0, 0))
+textRectr = textv2.get_rect()
+textRectr.center = (600, 450)
+
+textq = fontSmall.render("Press Q to quit", True, (0, 0, 0))
+textRectq = textv2.get_rect()
+textRectq.center = (950, 450)
 
 
-class Paddle1:
+class Paddle:
     def __init__(self, x, y, width, height):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
+        self.velY = 0
+        self.up_pressed = False
+        self.down_pressed = False
+        self.speed = 4
 
     def paddle(self):
         return pygame.Rect(self.x, self.y, self.width, self.height)
 
+    def update(self):
+        self.velY = 0
+        if self.up_pressed and not self.down_pressed:
+            self.velY = -self.speed
+        if self.down_pressed and not self.up_pressed:
+            self.velY = self.speed
 
-# class Paddle2:
-#     def __init__(self, x, y, width, height):
-#         self.x = x
-#         self.y = y
-#         self.width = width
-#         self.height = height
+        self.y += self.velY
+        if self.y >= SC_HEIGHT:
+            self.y = SC_HEIGHT
 
-#     def paddle(self):
-#         return pygame.Rect(self.x, self.y, self.width, self.height)
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
 
 
 class Ball:
@@ -93,21 +136,45 @@ class Ball:
         self.y += self.speed
 
 
-class Pause:
-    def __init__(self, x, y):
+class PU_invincible:
+    def __init__(self, x, y, width, height):
         self.x = x
         self.y = y
+        self.width = width
+        self.height = height
+
+    def ball(self):
+        return pygame.Rect(self.x, self.y, self.width, self.height)
+
+    def move(self):
+        self.speed = 5
+        self.x += self.speed
+        self.y += self.speed
+
+
+class Pause:
+    def __init__(self, x1, y1, x2, y2):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
 
     def pause(self):
-        textp = fontMedium.render("Paused", True, (0, 0, 0))
-        textRectp = textp.get_rect()
-        textRectp.center = (self.x, self.y)
-        screen.blit(logo, (600, 250))
-        screen.blit(textp, textRectp)
+        self.textp = fontMedium.render("Paused", True, (0, 0, 0))
+        self.textRectp = self.textp.get_rect()
+        self.textRectp.center = (self.x1, self.y1)
+
+        self.text_resume = fontSmall.render("Press E to resume", True, (0, 0, 0))
+        self.textRect_resume = self.text_resume.get_rect()
+        self.textRect_resume.center = (self.x2, self.y2)
+
+        screen.blit(smallerlogo, (440, 80))
+        screen.blit(self.textp, self.textRectp)
+        screen.blit(self.text_resume, self.textRect_resume)
 
 
-# Variable to keep our game loop running
-running = True
+pa1 = Paddle(p1x, p1y, p1w, p1h)
+pa2 = Paddle(p2x, p2y, p2w, p2h)
 
 # game loop
 while running:
@@ -115,66 +182,54 @@ while running:
 
     for event in pygame.event.get():
         keys = pygame.key.get_pressed()
-        pause = Pause(600, 350)
-        if keys[pygame.K_s]:
-            p1y += pspeed
-        if keys[pygame.K_w]:
-            p1y -= pspeed
-        if keys[pygame.K_k]:
-            p2y += pspeed
-        if keys[pygame.K_i]:
-            p2y -= pspeed
-        # if keys[pygame.K_ESCAPE]:
-        #     pause.pause()
-        #     BAXM = 0
-        #     BAYM = 0
-        #     pygame.display.update()
+        pause = Pause(600, 350, 600, 450)
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_s:
+                pa1.down_pressed = True
+            if event.key == pygame.K_w:
+                pa1.up_pressed = True
+            if event.key == pygame.K_k:
+                pa2.down_pressed = True
+            if event.key == pygame.K_i:
+                pa2.up_pressed = True
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_s:
+                pa1.down_pressed = False
+            if event.key == pygame.K_w:
+                pa1.up_pressed = False
+            if event.key == pygame.K_k:
+                pa2.down_pressed = False
+            if event.key == pygame.K_i:
+                pa2.up_pressed = False
 
     halfcourt = pygame.Rect(0, 0, 600, 700)
     screen.fill(background_colour)
-    pa1 = Paddle1(p1x, p1y, p1w, p1h)
-    pa2 = Paddle1(p2x, p2y, p2w, p2h)
     ball = Ball(ballx, bally, ballw, ballh)
+    pu_invincible = PU_invincible(pux, puy, puw, puh)
     pygame.draw.rect(screen, (255, 255, 153), halfcourt)
     pygame.draw.rect(screen, paddle1_colour, pa1.paddle())
     pygame.draw.rect(screen, paddle2_colour, pa2.paddle())
     pygame.draw.rect(screen, ball_colour, ball.ball())
 
-    text1 = fontMedium.render(str(score1), True, (255, 154, 246))
-    textRect1 = text1.get_rect()
-    textRect1.center = (400, 50)
-
-    text2 = fontMedium.render(str(score2), True, (255, 255, 253))
-    textRect2 = text2.get_rect()
-    textRect2.center = (800, 50)
-
-    textv1 = fontMedium.render("Player 1 has won. Woohoo.", True, (0, 0, 0))
-    textRectv1 = textv1.get_rect()
-    textRectv1.center = (600, 350)
-
-    textv2 = fontMedium.render("Player 2 has won. Yippee", True, (0, 0, 0))
-    textRectv2 = textv2.get_rect()
-    textRectv2.center = (600, 350)
-
-    textr = fontSmall.render("Press X to replay", True, (0, 0, 0))
-    textRectr = textv2.get_rect()
-    textRectr.center = (600, 450)
-
-    textq = fontSmall.render("Press ESC to quit", True, (0, 0, 0))
-    textRectq = textv2.get_rect()
-    textRectq.center = (950, 450)
-
     screen.blit(text1, textRect1)
     screen.blit(text2, textRect2)
 
-    if p1y <= 0:
-        p1y = 0
-    if p1y >= SC_HEIGHT - p1h:
-        p1y = SC_HEIGHT - p1h
-    if p2y <= 0:
-        p2y = 0
-    if p2y >= SC_HEIGHT - p2h:
-        p2y = SC_HEIGHT - p2h
+    prob = random.random()
+    if prob < 0.01:
+        spawn = True
+
+    if spawn:
+        pygame.draw.rect(screen, ball_colour, pu_invincible.ball())
+
+    if pa1.y <= 0:
+        pa1.y = 0
+    if pa1.y >= SC_HEIGHT - p1h:
+        pa1.y = SC_HEIGHT - p1h
+    if pa2.y <= 0:
+        pa2.y = 0
+    if pa2.y >= SC_HEIGHT - p2h:
+        pa2.y = SC_HEIGHT - p2h
 
     if bally < ballh:
         # ball has hit the top
@@ -183,6 +238,15 @@ while running:
     if bally > SC_HEIGHT - ballh:
         # ball has hit the bottom
         BAYM *= -1
+        pygame.mixer.Sound.play(blip)
+
+    if puy < puh:
+        # ball has hit the top
+        PUYM *= -1
+        pygame.mixer.Sound.play(blip)
+    if puy > SC_HEIGHT - puh:
+        # ball has hit the bottom
+        PUYM *= -1
         pygame.mixer.Sound.play(blip)
 
     # print(ballx)
@@ -200,12 +264,17 @@ while running:
         pygame.display.update()
 
     k = ball.ball()
+    pu1 = pu_invincible.ball()
     p1rect = pa1.paddle()
     p2rect = pa2.paddle()
     kpos = (k.x, k.y)
+    pu1pos = (pu1.x, pu1.y)
 
     collide1 = p1rect.collidepoint(kpos)
     collide2 = p2rect.collidepoint(kpos)
+
+    pu1collide1 = p1rect.collidepoint(pu1pos)
+    pu1collide2 = p2rect.collidepoint(pu1pos)
 
     if collide1:
         BAXM *= -1
@@ -214,8 +283,18 @@ while running:
         BAXM *= -1
         pygame.mixer.Sound.play(blip)
 
+    if pu1collide1:
+        PUXM *= -1
+        pygame.mixer.Sound.play(blip)
+    if pu1collide2:
+        PUXM *= -1
+        pygame.mixer.Sound.play(blip)
+
     ballx = ballx + BAXM
     bally = bally + BAYM
+
+    pux = pux + PUXM
+    puy = puy + PUYM
 
     if keys[pygame.K_ESCAPE]:
         PFUNC += 1
@@ -225,14 +304,16 @@ while running:
         pause.pause()
         BAXM = 0
         BAYM = 0
-        pspeed = 0
+        pa1.up_pressed = False
+        pa1.down_pressed = False
+        pa2.down_pressed = False
+        pa2.up_pressed = False
         # pygame.display.update()
         if keys[pygame.K_e]:
             PAUSED = False
             # pygame.display.update()
-            BAXM = 1
-            BAYM = 1
-            pspeed = 20
+            BAXM = 3
+            BAYM = 3
 
     if score1 == 10:
         BAXM = 0
@@ -241,14 +322,15 @@ while running:
         screen.blit(textv1, textRectv1)
         screen.blit(textr, textRectr)
         screen.blit(textq, textRectq)
+        screen.blit(smallerlogo, (440, 80))
         # pygame.mixer.Sound.play(victory)
         if keys[pygame.K_x]:
             score1 = 0
             score2 = 0
-            BAXM = 1
-            BAYM = 1
+            BAXM = 3
+            BAYM = 3
             pspeed = 20
-        if keys[pygame.K_ESCAPE]:
+        if keys[pygame.K_q]:
             running = False
 
     if score2 == 10:
@@ -258,18 +340,20 @@ while running:
         screen.blit(textv2, textRectv2)
         screen.blit(textr, textRectr)
         screen.blit(textq, textRectq)
+        screen.blit(smallerlogo, (440, 80))
         # pygame.mixer.Sound.play(victory)
         if keys[pygame.K_x]:
             score1 = 0
             score2 = 0
-            BAXM = 1
-            BAYM = 1
+            BAXM = 2
+            BAYM = 0.75
             pspeed = 0
-        if keys[pygame.K_ESCAPE]:
+        if keys[pygame.K_q]:
             running = False
 
-    if p1y >= SC_HEIGHT - p1h:
-        p1y = SC_HEIGHT - p1h
+    pa1.update()
+    pa2.update()
+    clock.tick(120)
     # Check for QUIT event
     pygame.display.update()
     if event.type == pygame.QUIT:
